b1fb1b6b2e3eab7706b9d15e2de62b10514d0d81
angga <anggadisaputra3@gmail.com>
2026-08-29 09:55:39 +0800

    menambahkan fitur javascript dan menambahkan file gambar serta menghapus folder project form-03.

---
+++ form-login-2/form-04/app.js
@@ -0,0 +1,18 @@
+const togglePassword = document.querySelector('#togglePassword');
+const passwordInput = document.querySelector('#password');
+const eyeIcon = document.querySelector('#eyeIcon');
+
+if (togglePassword && passwordInput) {
+    togglePassword.addEventListener('click', function () {
+        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordInput.setAttribute('type', type);
+
+        // === ganti icon mata ===
+        if (type === 'text') {
+            eyeIcon.setAttribute('data-feather', 'eye-off');
+        } else {
+            eyeIcon.setAttribute('data-feather', 'eye');
+        }
+        feather.replace();
+    });
+}
